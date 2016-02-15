1247c4ff596687da4f070c63adcca7c03fbbe43d
vidde <johanna.julen@gmail.com>
2016-02-15 19:01:19 +0100

    Started fixing things

--- globals.php
+++ globals.php
@@ -6,45 +6,20 @@
 //Slugs need to be untranslated or coder will have hell in case of many languages in translation!
 define('CUSTOM_PAGES_ARRAY',serialize(array(
 	//name	=>	content
-	_("Page without sub pages")			=>	array(	"slug"				=>	"head_page",
-													"req_user_level"	=>	0, //If 1, only logged in users can see this. If >=2 only users with this admin level can see this link.
-																				//NOTE: This number only affects the visibility of the link. The contents has to be governed separately
-													"meta_description"	=> "This page is an example of page without sub pages"
-												),
-	_("Admin tools")					=>	array(	"slug"				=>	"admin", //This will only affect the admin-menu! Subpages will be added to the regular admin menu.
-																						// For this condition to be true, slug needs to be exactly "admin". req_user_level is not nessessary
+	_("Edit")							=>	array(	"slug"	=>	"edit",
+													"req_user_level"	=>	5,
 													"subpages"		=>	array(
-																				_("Things 1")	=>	array(	"slug"	=>	"things1",
-																											"req_user_level"	=>	5	//This will be seen by only highest level of admin
+																				_("Tasks")	=>	array(	"slug"	=>	"tasks",
+																											"req_user_level"	=>	5	//This will be seen by logged in people
 																										),
-																				_("Things 2")	=>	array(	"slug"	=>	"things2",
+																				_("Categories")	=>	array(	"slug"	=>	"cat",
 																											"req_user_level"	=>	2	//This will be seen by admins
 																										)
 																			)
 												),
-	_("Things")							=>	array(	"slug"	=>	"things",
-													"req_user_level"	=>	1,
+	_("Admin")					=>	array(	"slug"				=>	"admin", //This will only affect the admin-menu! Subpages will be added to the regular admin menu.
+													// "req_user_level"	=>	5,		// For this condition to be true, slug needs to be exactly "admin". req_user_level is not nessessary
 													"subpages"		=>	array(
-																				_("Things 1")	=>	array(	"slug"	=>	"things1",
-																											"req_user_level"	=>	1	//This will be seen by logged in people
-																										),
-																				_("Things 2")	=>	array(	"slug"	=>	"things2",
-																											"req_user_level"	=>	2	//This will be seen by admins
-																										)
-																			)
-												),
-	_("Stuff")							=>	array(	"slug"	=>	"stuff",
-													"req_user_level"	=>	0,
-													"subpages"		=>	array(
-																				_("Active users")	=>	array(	"slug"	=>	"users",
-																											"req_user_level"	=>	0	//This will be seen by all
-																										),
-																				_("Stuff 2")	=>	array(	"slug"	=>	"stuff2",
-																											"req_user_level"	=>	2	//This will be seen by admins
-																										),
-																				_("Stuff 3")	=>	array(	"slug"	=>	"stuff3",
-																											"req_user_level"	=>	0	
-																										)
 																			)
 												)
 )));
@@ -52,7 +27,7 @@ define('CUSTOM_PAGES_ARRAY',serialize(array(
 /********************************/
 /*		Extra settings			*/
 /********************************/
-define('CUSTOM_SETTINGS',serialize(array(
-	"flattr"	=>	array(	"some_object" => _("Some object"))
-	)));
+// define('CUSTOM_SETTINGS',serialize(array(
+	// "flattr"	=>	array(	"some_object" => _("Some object"))
+	// )));
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
 
-display_topline_menu(); ?>
-		
-<div class="container" id="main_container">
-	<div id="content">
-	<?php 
-		display_conditional_login();
-	?>
-	<?php 
+display_topline_menu("navbar-default"); ?>
+
 	
-	include("content.php"); ?>
-	<div class="clearfix"></div>
+<div class="container" id="main_container">
+	
+	<div id="content" class="row">
+		<?php 
+			display_conditional_login();
+		?>
+		<?php 
+		
+		include("content.php"); ?>
+		<div class="clearfix"></div>
 	</div>
 
 	<?php //Footer
 	display_footer(); ?>
+	
+	
 	
 </div><!-- /.container -->
